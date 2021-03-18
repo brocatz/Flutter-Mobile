9a3d2143aec87d4b26277acb57b12a7ec974c292
Brocatz <brocatzcat@gmail.com>
2021-03-18 06:54:06 -0400

    Change the List to LinkedHashMap like the Cart

--- lib/models/Orders.dart
+++ lib/models/Orders.dart
@@ -1,10 +1,13 @@
+import 'dart:collection';
+
 import 'package:flutter_form/models/RestaurantMenuItemModel.dart';
 import 'package:flutter_form/models/UserModel.dart';
 
+// This class is an intermediate class Between User and FoodItems
 class Orders {
   int confirmationNumber; // The Id;
   DateTime dateFoodOrder;
 
-  List<RestaurantMenuItemModel> listItemOrder;
+  LinkedHashMap<RestaurantMenuItemModel, int> listItemOrder;
   UserModel user;
 }
