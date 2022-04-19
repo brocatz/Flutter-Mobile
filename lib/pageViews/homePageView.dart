d6f6650c8a6b3019772a23a1d664aa4a67bfd822
Brocatz <brocatzcat@gmail.com>
2022-04-19 13:25:00 -0400

    Added Flutter Google Ad Mod

--- lib/pageViews/homePageView.dart
+++ lib/pageViews/homePageView.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form/constant/ConstantTest.dart';
 import 'package:flutter_form/widgets/discoverMenuItem.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class HomePageView extends StatefulWidget {
   @override
@@ -8,6 +9,30 @@ class HomePageView extends StatefulWidget {
 }
 
 class _HomePageViewSate extends State<HomePageView> {
+  BannerAd _bannerAd;
+  bool _isAdLoaded = false;
+
+  @override
+  void initState() {
+    _initBannerAd();
+  }
+
+  void _initBannerAd() {
+    _bannerAd = new BannerAd(
+        size: AdSize.banner,
+        adUnitId: BannerAd.testAdUnitId,
+        listener: AdListener(
+            onAdLoaded: (ad) {
+              setState(() {
+                _isAdLoaded = true;
+              });
+            },
+            onAdFailedToLoad: (ad, error) {}),
+        request: AdRequest());
+
+    _bannerAd.load();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -18,18 +43,39 @@ class _HomePageViewSate extends State<HomePageView> {
           // This is suppose to fetch a network request
           return Future.delayed(Duration(seconds: 4));
         },
-        child: ListView.builder(
-          padding: EdgeInsets.only(
-            top: 10,
-            left: 20,
-            right: 20,
-          ),
-          itemCount: foodList.length,
-          itemBuilder: (context, index) {
-            return DiscoverMenuItem(
-              restaurantMenuItemModel: foodList[index],
-            );
-          },
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.only(
+                  top: 10,
+                  left: 20,
+                  right: 20,
+                ),
+                itemCount: foodList.length + 1,
+                itemBuilder: (context, index) {
+                  if (index == foodList.length) {
+                    return Container(
+                      margin: EdgeInsets.only(bottom: 20, top: 20),
+                      height: _bannerAd.size.height.toDouble(),
+                      width: _bannerAd.size.width.toDouble(),
+                      child: AdWidget(ad: _bannerAd),
+                    );
+                  }
+                  return DiscoverMenuItem(
+                    restaurantMenuItemModel: foodList[index],
+                  );
+                },
+              ),
+            ),
+            // _isAdLoaded
+            //     ? Container(
+            //         height: _bannerAd.size.height.toDouble(),
+            //         width: _bannerAd.size.width.toDouble(),
+            //         child: AdWidget(ad: _bannerAd),
+            //       )
+            //     : Text("")
+          ],
         ),
       ),
     );
